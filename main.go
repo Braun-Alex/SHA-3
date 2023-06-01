c2ae056d818274c0a032160eec40cbce14d6fabc
Braun_Alex <lamanasakasa@gmail.com>
2023-06-01 15:08:09 +0300

    Converting of bytes to bits has been added

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func sha3(data []byte) {
+	bits := make([]bool, 8*len(data))
+	for i, byteValue := range data {
+		for j := 0; j < 8; j++ {
+			bits[8*i+j] = (byteValue & (1 << byte(7-j))) == 1
+		}
+	}
+}
+
 func main() {
 	fmt.Print("AAA")
 }
