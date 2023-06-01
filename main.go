521439344cef206350ac6e9c5c53eea0121d98d2
Braun_Alex <lamanasakasa@gmail.com>
2023-06-01 17:55:48 +0300

    The theta function has been added

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 const b int = 1600
 const rate int = 576
+const rounds int = 24
+const w = 64
 
 func sha3(data []byte) {
 	countOfBits := 8 * len(data)
@@ -52,6 +54,44 @@ func sha3(data []byte) {
 		}
 		for j := 0; j < b; j++ {
 			state[j] = (state[j] && !iterationBlock[j]) || (!state[j] && iterationBlock[j])
+		}
+	}
+}
+
+func keccakPermutation(state [b]bool) {
+	var A [5][5][w]bool
+	for x := 0; x < 5; x++ {
+		for y := 0; y < 5; y++ {
+			for z := 0; z < w; z++ {
+				A[x][y][z] = state[w*(5*y+x)+z]
+			}
+		}
+	}
+}
+
+func theta(A [5][5][w]bool) {
+	var C [5][w]bool
+	for x := 0; x < 5; x++ {
+		for z := 0; z < w; z++ {
+			C[x][z] = (A[x][0][z] && !A[x][1][z]) || (!A[x][0][z] && A[x][1][z])
+			for y := 2; y < 5; y++ {
+				C[x][z] = (C[x][z] && !A[x][y][z]) || (C[x][z] && A[x][y][z])
+			}
+		}
+	}
+	var D [5][w]bool
+	for x := 0; x < 5; x++ {
+		for z := 0; z < w; z++ {
+			D[x][z] = (C[(x-1)%5][z] && !C[(x+1)%5][(z-1)%w]) ||
+				(!C[(x-1)%5][z] && C[(x+1)%5][(z-1)%w])
+		}
+	}
+	var R [5][5][w]bool
+	for x := 0; x < 5; x++ {
+		for y := 0; y < 5; y++ {
+			for z := 0; z < w; z++ {
+				R[x][y][z] = (A[x][y][z] && !D[x][z]) || (A[x][y][z] && D[x][z])
+			}
 		}
 	}
 }
